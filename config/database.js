7c1c14c044616ae297846cec1a4e2fd69935aba7
Olufemi Alabi <femdomsteve@yahoo.com>
2021-09-01 11:20:55 +0100

    Strapi development server setup and linked and configured to MongoDB

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'cluster0.gluvj.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'orgMgr'),
+        username: env('DATABASE_USERNAME', 'your-username'),
+        password: env('DATABASE_PASSWORD', 'your-password'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '74954b88-f011-40f7-9b01-03bcddf92c1f'
+};
